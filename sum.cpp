880ee7794eb369ef372ea0019ffb36648f2dd370
lblauto <lebaoloc76@gmail.com>
2024-03-15 18:25:39 +0700

    first commit

---
+++ sum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    int a, b;
+    cout << "Nhap so thu nhat: ";
+    cin >> a;
+    cout << "Nhap so thu hai: ";
+    cin >> b;
+
+    int sum = a + b;
+    cout << "Tong cua hai so ban vua nhap la: " << sum <<endl;
+
+    return 0;
+}
